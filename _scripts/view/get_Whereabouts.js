40cf8883671a20a904b89aa7176a5167764e3870
Mike Sackton <msackton@eschergroup.com>
2023-01-19 13:59:01 -0500

    * Fix date comparision logic * Fix boolean logic for home/origin and home/current comparison logic

--- _scripts/view/get_Whereabouts.js
+++ _scripts/view/get_Whereabouts.js
@@ -1,5 +1,20 @@
 function get_Whereabouts(metadata, input) {
    
+    // this converts a yamlDate to a string and a jsDate to a string and then compares them as strings
+    // returns 1 if yaml is greater than js, 0 if equal, -1 if less
+    function compare_dates(yamlDate, jsDate) {
+        if (!yamlDate) console.log("no yaml date");
+        if (!jsDate) console.log("no yaml date");
+
+        let yamlDateStr = yamlDate.year.toString().padStart(4, '0') + yamlDate.month.toString().padStart(2,'0') + yamlDate.day.toString().padStart(2,'0');
+        let jsDateStr = jsDate.getUTCFullYear().toString().padStart(4, '0') + (jsDate.getUTCMonth()+1).toString().padStart(2,'0') + jsDate.getUTCDate().toString().padStart(2,'0');
+
+        console.log("yamldate: " + yamlDateStr + " jsDate: " + jsDateStr);
+
+        if (yamlDateStr == jsDateStr) return 0;
+        return yamlDateStr > jsDateStr ? 1 : -1;
+    }
+
     function get_displayDateFromYaml(yamlDate) {         
         return get_displayDate(yamlDate.year, yamlDate.month-1, yamlDate.day);
     }
@@ -62,9 +77,12 @@ function get_Whereabouts(metadata, input) {
     if (metadata.whereabouts) {
         if (metadata.born > currentYear) return "";
 
-        let bornDate = Date.UTC(1, 0, 0, 0, 0, 0, 0);
+        let bornDate = new Date(1, 0, 0, 0, 0, 0, 0);
+        bornDate.setFullYear(1);
+        bornDate.setMonth(0);
+        bornDate.setDate(1);
         if (metadata.born) {
-            bornDate = Date.UTC(metadata.born, 0, 0, 0, 0, 0, 0);
+            bornDate.setFullYear(metadata.born);            
         }
       
         let lastSeen = undefined;
@@ -80,12 +98,17 @@ function get_Whereabouts(metadata, input) {
         }
 
         if (lastSeenByPartyDate) {
-            lastSeen = metadata.whereabouts.findLast(s => s.date <= lastSeenByPartyDate);
+            lastSeen = metadata.whereabouts.findLast(s => compare_date(s.date,lastSeenByPartyDate) <= 0);
         }
 
-        let current = metadata.whereabouts.findLast(s => s.date <= currentJsDate);
-        let home = metadata.whereabouts.findLast(s => s.date <= currentJsDate && !s.excursion);
-        let original = metadata.whereabouts.find(s => s.date >= bornDate);
+        let current = metadata.whereabouts.findLast(s => compare_dates(s.date, currentJsDate) <= 0);
+        let home = metadata.whereabouts.findLast(s => compare_dates(s.date, currentJsDate) <= 0 && !s.excursion);
+        let original = metadata.whereabouts.find(s => compare_dates(s.date, bornDate) >= 0);
+
+
+        if (home) console.log("home: " + home.date.year + " " + home.date.month + " " + home.date.day);
+        if (original) console.log("original: " + original.date.year + " " + original.date.month + " " + original.date.day);
+        if (current) console.log("current: " + current.date.year + " " + current.date.month + " " + current.date.day);
 
         let outputString = "";
 
@@ -101,7 +124,7 @@ function get_Whereabouts(metadata, input) {
 
         if (original) {
             // we have an original place -- show it if it doesn't match home place
-            if (original.place != home.place && original.region != home.region) {
+            if (original.place != home.place || original.region != home.region) {
                 outputString = input.prefix + "Originally from: " + get_Location(original.place, original.region) + input.suffix + "\n";
             }
         }
@@ -117,7 +140,7 @@ function get_Whereabouts(metadata, input) {
 
         if (current) {
             // we have a current place -- show if it doesn't match home
-            if (current.place != home.place && current.region != home.region) {
+            if (current.place != home.place || current.region != home.region) {
                 outputString += input.prefix + "Current Location (as of " + get_currentDisplayDate() + "): " + get_Location(current.place, current.region) + input.suffix + "\n";
             }
         }
