5c433dd3830a5e04644740efe79154ae63bd00f5
Mike Sackton <msackton@eschergroup.com>
2023-12-03 21:55:18 -0500

    some initial work on new name objects

--- _scripts/customJS/nameManager.js
+++ _scripts/customJS/nameManager.js
@@ -298,7 +298,251 @@ class NameManager {
         return article.trim()
     }
 
+
+    // this formats a name object given the format options and returns a string
+    // name = { name: the actual name, linkTarget: the target file, if there is one, article: the article, linkText: the linkText }
+
+    formatName(name, formatSpecifier) {
+
+        function toFirstUpper(inStr) {
+            if (!inStr) return inStr
+            let newStr = inStr.trim();
+            return newStr.charAt(0).toUpperCase() + newStr.slice(1);
+        }
+
+        if (!name || !name.name) {
+            return ""
+        }
+
+        // the format looks like: [linkText] [article] [[file|alias]]
+
+        let formattedName = "";
+        let specifier = formatSpecifier ?? ""
+        let initialUpper = specifier.includes("u")
+
+        // we include the link text if we (a) have a q but (b) do not have a negation (Q)
+        if (specifier.includes("q") && !specifier.includes("Q") && name.linkText) {
+            let linkText = name.linkText ?? ""
+            if (initialUpper) {
+                linkText = toFirstUpper(linkText)
+                initialUpper = false;
+            }
+
+            formattedName = linkText + " "
+        }
+
+
+        if (specifier.includes("a")) {
+            let article = name.indefiniteArticle ?? ""
+            // title case expects the article to be upper
+            if (initialUpper || specifier.includes("t")) {
+                article = toFirstUpper(article)
+                initialUpper = false;
+            }
+
+            formattedName += article + " "
+        }
+        else if (!specifier.includes("x") && name.definiteArticle) {
+            let article = name.definiteArticle ?? ""
+            // title case expects the article to be upper
+            if (initialUpper || specifier.includes("t")) {
+                article = toFirstUpper(article)
+                initialUpper = false;
+            }
+
+            formattedName += article + " "
+        }
+
+        let nameStr = name.name
+        if (specifier.includes("s")) {
+            nameStr = nameStr.toLowerCase()
+        } else if (specifier.includes("t")) {
+            nameStr = this.#toTitle(nameStr)
+        }
+
+        if (initialUpper) {
+            nameStr = toFirstUpper(nameStr)
+            nameStr = false;
+        }
+
+        // avoid creating links in certain cases
+        if (specifier.includes("y" && !name.linkTarget)) {
+            if (target.includes(",")) specifier = specifier.replace("y", "")
+            let fragmentsThatDontAlwaysLink = this.#getElementFromMetadata("fragmentsThatDontAutoLink")
+            if (fragmentsThatDontAlwaysLink) {
+                for (let word of nameStr.split(' ')) {
+                    if (fragmentsThatDontAlwaysLink.includes(word.toLowerCase())) {
+                        specifier = specifier.replace("y", "")
+                        break
+                    }
+                }
+            }
+        }
+
+        // if we have a y we always generate a link
+        if (specifier.includes("y")) {
+            if (!name.linkTarget || name.linkTarget == nameStr) {
+                formattedName += "[[" + nameStr + "]]"
+            } else {
+                // we have a link target that doesn't match
+                formattedName += "[[" + name.linkTarget + "|" + nameStr + "]]"
+            }
+        } else if (specifier.includes("n")) {
+            // never link
+            formattedName += nameStr
+        } else {
+            // link if we have a target, not otherwise
+            if (!name.linkTarget) {
+                formattedName += nameStr
+            } else if (name.linkTarget == nameStr) {
+                formattedName += "[[" + nameStr + "]]"
+            } else {
+                // we have a link target that doesn't match
+                formattedName += "[[" + name.linkTarget + "|" + nameStr + "]]"
+            }
+        }
+
+        return formattedName
+    }
+
+    #getLinkText(pageMetadata, sourceType) {
+        // given a page, and a origin page, get the link text describing the "link" between these pages
+        
+        let displayData = this.getDisplayData(pageMetadata)
+        
+        let prep = ""
+        switch (sourceType) {
+            case "person":
+                prep = displayData.ltPerson != undefined ? displayData.ltPerson : displayData.linkText
+                break
+            case "organization":
+                prep = displayData.ltOrg != undefined ? displayData.ltOrg : displayData.linkText
+                break
+            case "place":
+                prep = displayData.ltPlace != undefined ? displayData.ltPlace : displayData.linkText
+                break
+            case "item":
+                prep = displayData.ltItem != undefined ? displayData.ltItem : displayData.linkText
+                break
+            default:
+                prep = displayData.linkText
+        }
+
+        return prep ?? ""
+    }
+
+    #getIndefArt(inputString) {
+        
+        // indefinte article
+        let lowered = inputString.toLowerCase()        
+        if (lowered.startsWith("uni")) {
+            return "a"
+        } else if (lowered[0] == "a" || lowered[0] == "e" || lowered[0] == "i" || lowered[0] == "o" || lowered[0] == "u") {
+            return "an"
+        } else {
+            return "a"
+        }
+    }
+
+    #getDefArt(inputString) {
+        // given an input string this returns the article
+
+        if (!inputString) return ""
+        
+        if (/[A-Z~]/.test(inputString)) {
+            // capital letters, calculate a definite article
+            let wordCount = inputString.split(' ').length
+            if (wordCount > 1) return "the"
+            return ""
+        } else {
+            return "the"
+        }
+    }
+
+    // this returns a name object for a string
+    // name = { name: the actual name, linkTarget: the target file, if there is one, linkText: the linkText }
+    // overrides providers the option to replace the name itself, the article, or the linkText
+    //  { alias:, article:, linkText: }
+    getNameObject(target, sourceType, overrides) {
+
+        if (!overrides) overrides = {}
+
+        // this gets the canonical name of a potential link
+        if (!target || target == "Untitled") {
+            return {
+                name: overrides.alias ?? "",
+                linkTarget: undefined,
+                indefiniteArticle: "",
+                definiteArticle: "",
+                linkText: overrides.linkText ?? ""
+            }
+        }
+
+        let fileData = this.getFileForTarget(target)
+
+        if (!fileData) {
+            // we don't have a target
+            return {
+                name: overrides.alias ?? target,
+                linkTarget: undefined,
+                definiteArticle: this.#getDefArt(overrides.alias ?? target),
+                indefiniteArticle: this.#getIndefArt(overrides.alias ?? target),
+                linkText: ""
+            }
+        }
+
+        // we do have a target
+        let frontmatter = fileData.frontmatter
+        let selectedDescriptiveName = overrides.alias ?? (fileData.isAlias ? target : fileData.filename)
+        if (selectedDescriptiveName === fileData.filename) {
+            if (frontmatter.title && frontmatter.name) {
+                selectedDescriptiveName = frontmatter.title + " " + frontmatter.name
+            }
+            else if (frontmatter.name) {
+                selectedDescriptiveName = frontmatter.name
+            } else if (frontmatter.campaign && frontmatter.sessionNumber) {
+                selectedDescriptiveName = frontmatter.campaign + " " + frontmatter.sessionNumber
+            }
+        }
+
+        let displayDefaults = this.getDisplayData(frontmatter)
+        let defArt = ""
+        let indefArt = ""
+
+        if ("indefArt" in displayDefaults) {
+            indefArt = displayDefaults.indefArt ?? ""
+        } else {
+            indefArt = this.#getIndefArt(selectedDescriptiveName)
+        }
+        
+        
+        if ("defArt" in displayDefaults) {
+            defArt = displayDefaults.defArt ?? ""
+        } else {
+            defArt = this.#getDefArt(selectedDescriptiveName)
+        }
+
+        return {
+            name: selectedDescriptiveName,
+            definiteArticle: defArt,
+            indefiniteArticle: indefArt,
+            linkTarget: fileData.filename,
+            linkText: overrides.linkText ?? this.#getLinkText(fileData.frontmatter, sourceType)
+        }
+
+    }
+
+    // This returns a "name" for either a page or a string
+
     getName(target, format = "", alias = undefined, linkTextOverride = undefined, sourcePageType = undefined) {
+
+
+        let nameObject = this.getNameObject(target, sourcePageType, {
+            alias: alias,
+            linkText: linkTextOverride
+        })
+
+        return this.formatName(nameObject, format)
 
         // this gets the canonical name of a potential link
         if (!target || target == "Untitled") return undefined
