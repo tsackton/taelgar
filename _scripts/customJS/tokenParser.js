190d87d6106d231db1c5e915092931d9adb4899c
Tim Sackton <timsackton@gmail.com>
2023-12-04 00:56:55 -0500

    theoretically nearly complete Token Parser class

--- _scripts/customJS/tokenParser.js
+++ _scripts/customJS/tokenParser.js
@@ -1,11 +1,37 @@
 class TokenParser  {
 
     // This class is responsible for parsing tokens and returning formatted strings
-    // There are two main functions:
+    // There are three main functions:
     // 1. parseDisplayString: takes a display string with words and tokens ("This is a <token>, print it."), 
     //      replaces each token with a formatted version, and returns a formatted string
     // 2. getFormattedToken: takes a token string (<token:format>) and returns a formatted string
-    // Both functions take a string as the first argument, a file object as the second argument, and a targetDate as the third argument
+    // 3. formatToken: takes a token object and returns a formatted string
+    // Both functions take a string as the first argument, a file object as the second argument,
+    // a targetDate as the third argument, and an overrides object as the fourth argument
+
+    // Define the allowable filter and format characters
+    // unused characters: b c d e g h j k m v w z
+    // * format definitions * //
+    /*** 
+        t: title case; s: lower case, u: initial upper case
+        a: indefinite article, A: indefinite article if first, x: no article, q: preposition, Q: no preposition
+        n: never link, y: always link
+        !: prefer this format if possible
+    ***/
+
+    formatChars = "qQaAxnytsu!"
+
+    // * filter definitions * //
+    /*** 
+        R = include regions only; r = exclude regions (i.e. places with typeOf region)
+        L = include locations only; l = exclude locations
+        P = include people only; p = exclude people
+        I = include items only; i = exclude items
+        F = include first step only; f = exclude first step
+        O = include organizations only; o = exclude organizations
+    ***/
+    
+    filterChars = "rRpPlLiIoOfF!"
 
     #parseTokenString(input, filterChars, formatChars) {
 
@@ -102,48 +128,136 @@ class TokenParser  {
         return result;
     }
 
-    #getFormattedString(value, token) {
-        // currently just uses the NameManager to get a formatted string
-        return NameManager.getName(value, token.format)
+    #getParameterCaseInsensitive(object, key) {
+        // get case insensitive parameter from object
+        const asLowercase = key.toLowerCase();
+        return object[Object.keys(object)
+        .find(k => k.toLowerCase() === asLowercase)
+        ];
     }
 
-    #getMainType(metadata, typeOf) {
-        // returns the main type for a given metadata
-        pass
+    #getFormattedName(value, token, targetDate, metadata) {
+        // returns a formatted name
+        // pass the format and the name to the name manager to generate a formatted name
+
+        const { NameManager } = customJS;
+
+        alias = null
+        sourcePageType = null
+
+        // find alias //
+        if (metadata.alias) {
+            alias = metadata.alias
+        } else {
+            possibleAliasKey = token.token + "Alias"
+            alias = this.#getParameterCaseInsensitive(metadata, possibleAliasKey)
+        }
+
+        if (metadata.sourcePageType) {
+            sourcePageType = metadata.sourcePageType
+        } else {
+            sourcePageType = NameManager.getPageType(metadata)
+        }
+
+        return NameManager.getName(value, token.format, alias, metadata.linkText, sourcePageType)
     }
 
-    #getFormattedLocChain(value, token) {
+    #getFormattedLocChain(whereabout, token, targetDate, metadata) {
+
+        const { NameManager } = customJS;
+        const { LocationManager } = customJS;
+
         // returns a formatted location chain
-        pass
+        let followDate = targetDate
+
+        // set q as default if missing //
+        // not sure this is right //
+        if (!token.filter.includes("q")) {
+            token.filter += "q"
+        }
+
+        // if the away end date of the whereabout is before the target date, use that as the follow date
+        if (whereabout.awayEnd && targetDate && whereabout.awayEnd.sort < targetDate.sort) {
+            followDate = whereabout.awayEnd
+        }
+        if (whereabout.startFilter && !token.filter.includes("!")) {
+            token.filter = this.#parseTokenString("<:" + whereabout.startFilter + ">", this.filterChars, this.formatChars).filter
+        }
+
+        sourcePageType = NameManager.getPageType(metadata)
+
+        // this doesn't currently work because we have mindepth and maxdepth parsed but not used //
+        // need to refactor getCurrentLocationName to accept token object and pass along mindepth and maxdepth //
+
+        return LocationManager.getCurrentLocationName(whereabout, followDate, token.filter, sourcePageType, token.format, token.firstformat)
+
     }
 
-    #getFormattedDate(value, token) {
+    #getFormattedDate(value, token, targetDate, metadata) {
+
+        const { DateManager } = customJS;
+
         // returns a formatted date
         // right now this just normalizes the date and returns the display value
         return DateManager.normalizeDate(value).display
     }
 
-    #formatToken(token, file, targetDate) {
-        // Takes a token object and returns a formatted string
+    #getDefaultTypeOf(metadata) {
 
-        function getParameterCaseInsensitive(object, key) {
-            // get case insensitive parameter from object
-            const asLowercase = key.toLowerCase();
-            return object[Object.keys(object)
-            .find(k => k.toLowerCase() === asLowercase)
-            ];
+        if (metadata.typeOf) return metadata.typeOf
+        if ("typeOf" in metadata) return ""
+
+        if (metadata.tags && metadata.tags.length > 0) {
+            let baseTag = metadata.tags.filter(f => f.startsWith("item") || f.startsWith("place") || f.startsWith("organization") || f.startsWith("person")).first()
+            if (baseTag) {
+                let value = baseTag.split("/")
+                if (value.length == 2) return value[1]
+                return value[0]
+
+            }
         }
 
-        let metadata = file.frontmatter
+        return undefined;
+    }
+
+    formatToken(token, file, targetDate, overrides) {
+
+        const { NameManager } = customJS
+        const { WhereaboutsManager } = customJS
+        const { DateManager } = customJS
+        const { AffiliationManager } = customJS
+
+        // Takes a token object and returns a formatted string
+
+        // reused from https://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
+
+        function merge_options(obj1, obj2) {
+            var obj3 = {};
+            if (obj1) for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+            if (obj2) for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+            return obj3;
+        }
+
+        // overrides that are used elsewhere //
+        // overrides.sourcePageType sets the source type in name formatting (default otherwise is page type) //
+        // overrides.alias sets the alias in name formatting (default otherwise is metadata.<token.token>Alias) //
+        // overrides.linkText sets the link text in name formatting (default otherwise is computed by NameManager) //
+        // overrides.dateInfo replaces the dateInfo object that is computed by DateManager //
+
+        if (overrides) {
+            metadata = merge_options(metadata, overrides)
+        } else if (file.frontmatter) {
+            metadata = file.frontmatter
+        } else {
+            metadata = {}
+        }
+
         if (targetDate) targetDate = DateManager.normalizeDate(targetDate)
-        let pageDateInfo = DateManager.getPageDates(metadata, targetDate)
-        // condsider: consolidate displayData in Token Parser class, or make a new class to manage display strings
-        // not really related to name management?
+        let pageDateInfo = metadata.dateInfo ?? DateManager.getPageDates(metadata, targetDate)
         let displayDefaults = NameManager.getDisplayData(metadata)
 
-        // if token is not valid, return empty string
         let value = "";
-        
+
         // clean up tokens //
         // target, targetdate, and currentdate are all the same thing
         if (token.token == "target" || token.token == "currentdate") token.token = "targetdate"
@@ -198,12 +312,12 @@ class TokenParser  {
             // start string options //
             case "name":
                 value = file.name
-                formatter = "string"
+                formatter = "name"
                 break;
             case "maintype":
                 // special case where we define a specific main type based on metadata //
-                value = this.#getMainType(metadata);
-                formatter = "string"
+                value = metadata.species ?? this.#getDefaultTypeOf(metadata)
+                formatter = "name"
                 break;
             // end string options //
 
@@ -259,14 +373,14 @@ class TokenParser  {
                     value = ""
                 }
                 else {
-                    value = this.#getFormattedString("ka") + " " + (metadata.ka ?? "unknown")
+                    value = this.#getFormattedName("ka") + " " + (metadata.ka ?? "unknown")
                 }
                 formatter = "none"
                 break;
             // end complicated options //
 
             // REFACTOR OPTIONS //
-            // these need to be refactored but I don't really understand the affiliation code yet //
+            // these might need to be refactored but I don't really understand the affiliation code yet //
             case "primary":
                 // currently we just use the affliation manager here to get a fully formatted string
                 value = AffiliationManager.getFormattedPrimaryAffiliations(metadata, token, targetDate)
@@ -280,64 +394,40 @@ class TokenParser  {
 
             default:
                 // if no special processing, check to see if it is a key in metadata, or failing that, displayDefaults
-                value = (getParameterCaseInsensitive(metadata, token.token) ?? getParameterCaseInsensitive(displayDefaults, token.token)) ?? ""
+                value = (this.#getParameterCaseInsensitive(metadata, token.token) ?? this.#getParameterCaseInsensitive(displayDefaults, token.token)) ?? ""
                 // default formatter is string
-                formatter = "string"
+                formatter = "name"
             }
 
-        if (formatter == "string") {
-            return this.#getFormattedString(value, token, targetDate)
+        if (formatter == "name") {
+            return this.#getFormattedName(value, token, targetDate, metadata)
         } else if (formatter == "date") {
-            return this.#getFormattedDate(value, token)
+            return this.#getFormattedDate(value, token, targetDate, metadata)
         } else if (formatter == "locchain") {
-            return this.#getFormattedLocChain(value, token, targetDate)
+            return this.#getFormattedLocChain(value, token, targetDate, metadata)
         } else {
-            return value.desc
+            return value
         }
     }
 
-    getFormattedToken(input, file, targetDate) {
-        // Takes a string, which may contain a token, and returns a formatted string
+    getFormattedToken(input, file, targetDate, overrides) {
+        // Takes a string, which is a token, and returns a formatted string
         // If no token is present, returns the original string
 
         let resultString = input;
 
-        // Define the allowable filter and format characters
-        // unused characters: b c d e g h j k m v w z
-        // * format definitions * //
-        /*** 
-            t: title case; s: lower case, u: initial upper case
-            a: indefinite article, A: indefinite article if first, x: no article, q: preposition, Q: no preposition
-            n: never link, y: always link
-            !: prefer this format if possible
-        ***/
-
-        let formatChars = "qQaAxnytsu!"
-
-        // * filter definitions * //
-        /*** 
-            R = include regions only; r = exclude regions (i.e. places with typeOf region)
-            L = include locations only; l = exclude locations
-            P = include people only; p = exclude people
-            I = include items only; i = exclude items
-            F = include first step only; f = exclude first step
-            O = include organizations only; o = exclude organizations
-         ***/
-
-        let filterChars = "rRpPlLiIoOfF!";
-
         // Parse the token string
-        let token = this.#parseTokenString(input, filterChars, formatChars);
+        let token = this.#parseTokenString(input, this.filterChars, this.formatChars);
 
         // If the token is valid, get the formatted string
         if (token.token) {
-            resultString = this.#formatToken(token, file, targetDate);
+            resultString = this.formatToken(token, file, targetDate, overrides);
         }
 
         return resultString;
     }
 
-    parseDisplayString(input, file, targetDate) {
+    parseDisplayString(input, file, targetDate, overrides) {
         // takes a display string, splits on tokens, formats each, and returns a formatted string
         let pieces = input.trim().split(/\s+/)
         let formattedPieces = pieces.map(piece => {
@@ -349,7 +439,7 @@ class TokenParser  {
                 let afterToken = piece.substring(piece.indexOf(tokenMatch[0]) + tokenMatch[0].length);
     
                 // Pass the token to the token formatter
-                let formattedToken = this.getFormattedToken(tokenMatch[0], file, targetDate);
+                let formattedToken = this.getFormattedToken(tokenMatch[0], file, targetDate, overrides);
     
                 // Replace the token with its formatted value
                 return beforeToken + formattedToken + afterToken;
