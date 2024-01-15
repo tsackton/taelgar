7ad0789dc809e7a8dd9d83dc638f7f95a259ce77
Mike Sackton <msackton@eschergroup.com>
2024-01-14 20:23:10 -0500

    initial WIP

--- _scripts/templater/prepareForExport.js
+++ _scripts/templater/prepareForExport.js
@@ -1,16 +1,16 @@
 async function processLinksInFile(tfile, checkRooted, campaign, notice) {
     let root = window.app.vault.getRoot().path
 
-    const {NameManager} = customJS
-  
+    const { NameManager } = customJS
+
     notice.setMessage("Checking links in file\n\n" + tfile.name.padEnd(100, " "))
 
     console.log("Processing file " + tfile.name)
-   
-    let metadata = app.metadataCache.getFileCache(tfile)   
+
+    let metadata = app.metadataCache.getFileCache(tfile)
     if (checkRooted && metadata && metadata.frontmatter && metadata.frontmatter.rooted) return
-    
-    await app.vault.process(tfile, (data) => {        
+
+    await app.vault.process(tfile, (data) => {
         let currentContents = data.split('\n');
 
         // the end of the yaml -- this is 0-counting, so if the file is just a yaml start and end it will be 1
@@ -33,10 +33,10 @@ async function processLinksInFile(tfile, checkRooted, campaign, notice) {
         return currentContents.join("\n");
     })
 
-    if (metadata && metadata.frontmatter && metadata.frontmatter.excludeRooted) {           
+    if (metadata && metadata.frontmatter && metadata.frontmatter.excludeRooted) {
         if (metadata.frontmatter.excludeRooted.some(f => f === campaign)) return
     }
-    
+
     if (metadata && metadata.links) {
         for (let link of metadata.links) {
             // quick search            
@@ -55,7 +55,6 @@ async function prepareForExport(tp, headerType) {
 
     let date = await tp.system.prompt("Enter new fantasy calendar date override: (YYYY, YYYY-MM, or YYYY-MM-DD, or blank to clear)")
     let campaign = await tp.system.prompt("Enter a campaign key for rooted exclusion calculations: ")
-    let startingFile = await tp.system.prompt("Enter file to start rooted check from (leave blank to skip): ", tp.file.folder(true) + "/" + tp.file.title)
 
     let normalized = DateManager.normalizeDate(date)
 
@@ -70,16 +69,9 @@ async function prepareForExport(tp, headerType) {
     let processed = 0
     let errors = 0
 
-    if (startingFile) {
-        var fpN = new Notice("File link processing started", 0)
-        let mainFile = app.vault.getAbstractFileByPath(startingFile)
-        await processLinksInFile(mainFile, false, campaign, fpN)
-        fpN.hide()
-        var notice = new Notice("File link processing completed. Making headers static", 0)
-    } else {
-        var notice = new Notice("Link checking skipped. Making headers static", 0)
-    }
+    var notice = new Notice("Making headers static", 0)
     for (let i = 0; i < files.length; i++) {
+        break
         await app.vault.process(files[i], (data) => {
             let md = app.metadataCache.getFileCache(files[i])
             if (md && md.frontmatter && md.frontmatter.tags && md.frontmatter.headerVersion) {
@@ -104,6 +96,47 @@ async function prepareForExport(tp, headerType) {
     notice.hide()
 
     new Notice("Headers updated.")
+
+
+    var notice = new Notice("Making DataView queries static", 0)
+    for (let i = 0; i < files.length; i++) {
+        await app.vault.process(files[i], async (data) => {
+            let md = app.metadataCache.getFileCache(files[i])
+
+            const dataviewJsPrefix = "dataviewjs";
+            const dataViewJsRegex = new RegExp(`\`\`\`${escapeRegex(dataviewJsPrefix)}\\s(.+?)\`\`\``, "gsm");
+            const dataviewJsMatches = text.matchAll(dataViewJsRegex);
+
+            for (const queryBlock of dataviewJsMatches) {
+                try {
+                    const block = queryBlock[0];
+                    const query = queryBlock[1];
+
+                    const div = createEl("div");
+                    const component = new Component();
+                    await window.app.plugins.plugins.dataview.api.executeJs(query, div, component, path);
+                    component.load();
+                    const markdown = div.innerHTML
+                    replacedText = replacedText.replace(block, markdown);
+                } catch (e) {
+                    logs({ settings: properties.settings, e: true }, e);
+                    notif({ settings: properties.settings }, error);
+                    return queryBlock[0];
+                }
+            }
+
+
+            processed++
+            return data
+        })
+
+        notice.setMessage("Making DataView queries static\nProcessing file " + (files[i].name.padEnd(100, " ")) + "\n\n" + processed + " of " + files.length + "\n\nErrors: " + errors)
+    }
+
+    notice.hide()
+
+    new Notice("Headers updated.")
+
 
     customJS.state.overrideDate = undefined
 
