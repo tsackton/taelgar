9af8368ed54637175af6478c1f233c6248894d63
Mike Sackton <msackton@eschergroup.com>
2024-01-15 14:45:09 -0500

    Add activeYear to files that require it

--- _scripts/templater/prepareForExport.js
+++ _scripts/templater/prepareForExport.js
@@ -1,16 +1,16 @@
 async function processLinksInFile(tfile, checkRooted, campaign, notice) {
     let root = window.app.vault.getRoot().path
 
-    const {NameManager} = customJS
-  
+    const { NameManager } = customJS
+
     notice.setMessage("Checking links in file\n\n" + tfile.name.padEnd(100, " "))
 
     console.log("Processing file " + tfile.name)
-   
-    let metadata = app.metadataCache.getFileCache(tfile)   
+
+    let metadata = app.metadataCache.getFileCache(tfile)
     if (checkRooted && metadata && metadata.frontmatter && metadata.frontmatter.rooted) return
-    
-    await app.vault.process(tfile, (data) => {        
+
+    await app.vault.process(tfile, (data) => {
         let currentContents = data.split('\n');
 
         // the end of the yaml -- this is 0-counting, so if the file is just a yaml start and end it will be 1
@@ -33,10 +33,10 @@ async function processLinksInFile(tfile, checkRooted, campaign, notice) {
         return currentContents.join("\n");
     })
 
-    if (metadata && metadata.frontmatter && metadata.frontmatter.excludeRooted) {           
+    if (metadata && metadata.frontmatter && metadata.frontmatter.excludeRooted) {
         if (metadata.frontmatter.excludeRooted.some(f => f === campaign)) return
     }
-    
+
     if (metadata && metadata.links) {
         for (let link of metadata.links) {
             // quick search            
@@ -50,13 +50,10 @@ async function processLinksInFile(tfile, checkRooted, campaign, notice) {
 
 async function prepareForExport(tp, headerType) {
 
-    const { DateManager } = customJS
     const { OutputHandler } = customJS
+    const { DateManager } = customJS    
 
     let date = await tp.system.prompt("Enter new fantasy calendar date override: (YYYY, YYYY-MM, or YYYY-MM-DD, or blank to clear)")
-    let campaign = await tp.system.prompt("Enter a campaign key for rooted exclusion calculations: ")
-    let startingFile = await tp.system.prompt("Enter file to start rooted check from (leave blank to skip): ", tp.file.folder(true) + "/" + tp.file.title)
-
     let normalized = DateManager.normalizeDate(date)
 
     if (normalized) {
@@ -70,15 +67,8 @@ async function prepareForExport(tp, headerType) {
     let processed = 0
     let errors = 0
 
-    if (startingFile) {
-        var fpN = new Notice("File link processing started", 0)
-        let mainFile = app.vault.getAbstractFileByPath(startingFile)
-        await processLinksInFile(mainFile, false, campaign, fpN)
-        fpN.hide()
-        var notice = new Notice("File link processing completed. Making headers static", 0)
-    } else {
-        var notice = new Notice("Link checking skipped. Making headers static", 0)
-    }
+    var notice = new Notice("Link checking skipped. Making headers static", 0)
+
     for (let i = 0; i < files.length; i++) {
         await app.vault.process(files[i], (data) => {
             let md = app.metadataCache.getFileCache(files[i])
@@ -97,6 +87,19 @@ async function prepareForExport(tp, headerType) {
             processed++
             return data
         })
+
+        await app.fileManager.processFrontMatter(files[i], frontmatter => {
+            const { DateManager } = customJS
+  
+            if (!frontmatter) return
+            if ("activeYear" in frontmatter) return
+            
+            let pageDates = DateManager.getPageDates(frontmatter)
+            if (pageDates.startDate) {
+                frontmatter["activeYear"] = pageDates.startDate.year
+            }
+
+        });
 
         notice.setMessage("Making headers static\nProcessing file " + (files[i].name.padEnd(100, " ")) + "\n\n" + processed + " of " + files.length + "\n\nErrors: " + errors)
     }
