7a7e29bf292b91e50e84bc7b68bf3144a1c77513
Mike Sackton <msackton@eschergroup.com>
2024-01-15 15:28:53 -0500

    delete unused/default export date

--- _scripts/templater/prepareForExport.js
+++ _scripts/templater/prepareForExport.js
@@ -1,60 +1,21 @@
-async function processLinksInFile(tfile, checkRooted, campaign, notice) {
-    let root = window.app.vault.getRoot().path
-
-    const { NameManager } = customJS
-
-    notice.setMessage("Checking links in file\n\n" + tfile.name.padEnd(100, " "))
-
-    console.log("Processing file " + tfile.name)
-
-    let metadata = app.metadataCache.getFileCache(tfile)
-    if (checkRooted && metadata && metadata.frontmatter && metadata.frontmatter.rooted) return
-
-    await app.vault.process(tfile, (data) => {
-        let currentContents = data.split('\n');
-
-        // the end of the yaml -- this is 0-counting, so if the file is just a yaml start and end it will be 1
-        let indexOfYamlEnd = currentContents.findIndex((f, i) => i > 0 && f == "---");
-
-        if (indexOfYamlEnd > 0) {
-            let yaml = currentContents.filter((v, i) => i >= 0 && i < indexOfYamlEnd)
-            let sharedYamlIdx = yaml.findIndex(f => f.trim().startsWith("rooted"))
-            if (sharedYamlIdx >= 0) {
-                currentContents[sharedYamlIdx] = "rooted: true"
-            } else {
-                currentContents.splice(1, 0, "rooted: true")
-                indexOfYamlEnd++
-            }
-        } else {
-            currentContents.splice(0, 0, "---")
-            currentContents.splice(0, 0, "rooted: true")
-            currentContents.splice(0, 0, "---")
-        }
-        return currentContents.join("\n");
-    })
-
-    if (metadata && metadata.frontmatter && metadata.frontmatter.excludeRooted) {
-        if (metadata.frontmatter.excludeRooted.some(f => f === campaign)) return
-    }
-
-    if (metadata && metadata.links) {
-        for (let link of metadata.links) {
-            // quick search            
-            let tfile = window.app.metadataCache.getFirstLinkpathDest(link.link, root)
-            if (tfile) {
-                await processLinksInFile(tfile, true, campaign, notice)
-            }
-        }
-    }
-}
-
 async function prepareForExport(tp, headerType) {
 
     const { OutputHandler } = customJS
-    const { DateManager } = customJS    
+    const { DateManager } = customJS
 
-    let date = await tp.system.prompt("Enter new fantasy calendar date override: (YYYY, YYYY-MM, or YYYY-MM-DD, or blank to clear)")
+    let websiteDate = ""
+    try {
+        let metadataFile = await app.vault.adapter.read(app.vault.configDir + "../../website.json");
+        let websiteData = JSON.parse(metadataFile)
+        websiteDate = websiteData.export_date
+    }
+    catch { }
+
+    let defaultExport = websiteDate ?? ""
+
+    let date = await tp.system.prompt("Enter new fantasy calendar date override: (YYYY, YYYY-MM, or YYYY-MM-DD, or blank to clear)", defaultExport)
     let normalized = DateManager.normalizeDate(date)
+
 
     if (normalized) {
         customJS.state.overrideDate = normalized
@@ -90,10 +51,10 @@ async function prepareForExport(tp, headerType) {
 
         await app.fileManager.processFrontMatter(files[i], frontmatter => {
             const { DateManager } = customJS
-  
+
             if (!frontmatter) return
             if ("activeYear" in frontmatter) return
-            
+
             let pageDates = DateManager.getPageDates(frontmatter)
             if (pageDates.startDate) {
                 frontmatter["activeYear"] = pageDates.startDate.year
@@ -110,6 +71,6 @@ async function prepareForExport(tp, headerType) {
 
     customJS.state.overrideDate = undefined
 
-
+    //tp.user.terminate()
 }
 module.exports = prepareForExport;
